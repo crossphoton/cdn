db987b74c5e0133625f0564f30c7a3ed6fbfbd5f
Aditya Agrawal <38404790+crossphoton@users.noreply.github.com>
2020-05-25 15:42:16 +0530

    Update and rename index.js to NewsAPI.js

--- NewsAPI.js
+++ NewsAPI.js
@@ -1,3 +1,16 @@
+/**
+ * This module provides access to (NewsAPI.org)'s API
+ * http://newsapi.org
+ * 
+ * Refer to the README.md file to know how to use.
+ * https://github.com/crossphoton/newsapi-deno#readme
+ * 
+ * This module is covered under MIT license
+ * 
+ * The creator and maintainer of this module does not relate to NewsAPI.org in any manner and
+ */
+
+
 const host = 'https://newsapi.org/v2'
 
 const queryPoints = {
@@ -11,7 +24,7 @@ const queryPoints = {
     from: "from",
     to: "to",
     domains: "domains",
-    excludeDomains: "excludeDomains",
+    exclude: "excludeDomains",
     queryInTitle: "qInTitle",
     sortBy: "sortBy",
 }
@@ -41,6 +54,10 @@ export class NewsAPI{
 }
 
 
+
+/**
+ * Used for producing errors
+ */
 class ApiError extends Error{
     constructor(err){
         super();
@@ -49,36 +66,48 @@ class ApiError extends Error{
     }
 }
 
+class queryError extends Error{
+    constructor(err){
+        super();
+        this.name = 'Error Occured'
+        this.message = `term '${err.term}' is not supported. Refer the README file for more information.\ 
+        If you feel the module is out of date generate a issue at https://www.github.com/crossphoton/newsapi-deno`
+    }
+}
+
 class HTTPError extends Error {}
 
+
+/**
+ * Adds the API key to the string and use the fetch API to provide data fetched from the API
+ * @param {*} initialQuery 
+ * @param {*} API_KEY 
+ */
 const getData = async (initialQuery, API_KEY) => {
     let requestString = initialQuery + `&apiKey=${API_KEY}`
-    let tester;
+    console.log(requestString)
     let result = await fetch(requestString)
     if(!result.ok){
+        console.log("From error checker in function")
         throw new ApiError(result)
     }
     return result.json();
 }
 
+
+/**
+ * Creates the request URL without apiKey
+ * @param {*} initialQuery 
+ * @param {*} pointsObject 
+ */
 const makeQueryString = (initialQuery, pointsObject) => {
     let query = initialQuery
-    for(let point in pointsObject) 
+    for(let point in pointsObject){
+        if(!queryPoints[point]) throw new queryError({term: point})
         query += `${queryPoints[point]}=${pointsObject[point]}&`;
+    }
     query = query.substring(0, query.length-1);
     return query;
 }
-
-var test = host + '/top-headlines?'
-
-var testQuery = {
-    country: 'in',
-    query: "test"
-}
-
-// let testres = getData(makeQueryString(test, testQuery), "10fe6539e67344ff99a6d3b7b1dec530")
-//             .then(data => console.log(data))
-
-// console.log(testres)
 
 export default NewsAPI
